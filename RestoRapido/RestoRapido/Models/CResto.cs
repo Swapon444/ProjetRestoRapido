8832b2139ea8cc79a888b075a9ef9e90de7e5aca
Dave Otis <Otid91@gmail.com>
2016-02-20 01:09:06 -0500

    Ajout des validations de champs lors de la création de nouveaux restos

--- RestoRapido/RestoRapido/Models/CResto.cs
+++ RestoRapido/RestoRapido/Models/CResto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,10 +14,14 @@ namespace RestoRapido.Models
         [DisplayName("Nom du restaurant")]
         public string resNom { get; set; }
         [DisplayName("Code postal")]
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "Assurez-vous d'entrer un code postal complet")]
+        [RegularExpression(@"^[A-Z]\d[A-Z] \d[A-Z]\d$", ErrorMessage = "Le code postal doit être au format A1A 1A1")]
         public string resPostal { get; set; }
         [DisplayName("Rue")]
+        [RegularExpression(@"^[a-Z]*$", ErrorMessage = "Veuillez entrer des charactère valide")]
         public string resRue { get; set; }
         [DisplayName("Numéro Civil")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Veuillez entrer le numéro d'établissement")]
         public string resNoCiv { get; set; }
 
         public ICollection<CTable> Tables { get; set; }
