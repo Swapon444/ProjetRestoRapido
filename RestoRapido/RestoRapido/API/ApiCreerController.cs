3a83e23299c06d6377b28cb9fdccc0b54466d2e5
Antoine Monzerol <antoine00000@hotmail.com>
2016-03-10 12:49:28 -0500

    Correction de APILogin

--- RestoRapido/RestoRapido/API/ApiCreerController.cs
+++ RestoRapido/RestoRapido/API/ApiCreerController.cs
@@ -12,7 +12,7 @@ namespace RestoRapido.Controllers
 {
     public class ApiCreerController : ApiController
     {
-  
+        [HttpGet]
         public int Creer(string login, string mdp, string prenom, string nom, bool boBle, bool boLait, bool boOeuf, bool boArachide, bool boSoja, bool boFruitCoque, bool boPoisson, bool boSesame, bool boCrustace, bool boMollusque)
         {
             SqlConnection conn = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbRestoRapidoV26.mdf;Initial Catalog=RestoRapido;Integrated Security=True");
@@ -20,12 +20,13 @@ namespace RestoRapido.Controllers
             checkuser.Connection = conn;
             conn.Open();
             SqlDataReader dr = checkuser.ExecuteReader();
-            conn.Close();
+            
 
             if (!dr.HasRows)
             {
-            
-                SqlCommand adduser = new SqlCommand("INSERT INTO Utilisateurs VALUES (" + mdp + "," + login + "," + nom + "," + prenom + ", Client ," + boBle + "," + boLait + "," + boOeuf + "," + boArachide + "," + boSoja + "," + boFruitCoque + "," + boPoisson + "," + boSesame + "," + boCrustace + "," + boMollusque + ")", conn);
+                conn.Close();
+
+                SqlCommand adduser = new SqlCommand("INSERT INTO Utilisateurs VALUES ('" + CEncryption.CalculateMD5Hash(mdp) + "','" + login + "','" + nom + "','" + prenom + "','Client','" + boBle + "','" + boLait + "','" + boOeuf + "','" + boArachide + "','" + boSoja + "','" + boFruitCoque + "','" + boPoisson + "','" + boSesame + "','" + boCrustace + "','" + boMollusque + "')", conn);
                 adduser.Connection = conn;
                 conn.Open();
                 adduser.ExecuteReader();
