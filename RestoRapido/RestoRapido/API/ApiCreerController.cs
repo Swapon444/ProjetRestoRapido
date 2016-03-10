593c35c00802e9de3d4d9c9eb3186c89839eb902
AntoineMonzerol <antoine00000@hotmail.com>
2016-03-09 21:01:46 -0500

    Api pour création/modification de comptes

---
+++ RestoRapido/RestoRapido/API/ApiCreerController.cs
@@ -0,0 +1,40 @@
+﻿using RestoRapido.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace RestoRapido.Controllers
+{
+    public class ApiCreerController : ApiController
+    {
+  
+        public int Creer(string login, string mdp, string prenom, string nom, bool boBle, bool boLait, bool boOeuf, bool boArachide, bool boSoja, bool boFruitCoque, bool boPoisson, bool boSesame, bool boCrustace, bool boMollusque)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbRestoRapidoV26.mdf;Initial Catalog=RestoRapido;Integrated Security=True");
+            SqlCommand checkuser = new SqlCommand("SELECT * FROM Utilisateurs WHERE UtilisateurNomUsager = '" + login + "'", conn);
+            checkuser.Connection = conn;
+            conn.Open();
+            SqlDataReader dr = checkuser.ExecuteReader();
+            conn.Close();
+
+            if (!dr.HasRows)
+            {
+            
+                SqlCommand adduser = new SqlCommand("INSERT INTO Utilisateurs VALUES (" + mdp + "," + login + "," + nom + "," + prenom + ", Client ," + boBle + "," + boLait + "," + boOeuf + "," + boArachide + "," + boSoja + "," + boFruitCoque + "," + boPoisson + "," + boSesame + "," + boCrustace + "," + boMollusque + ")", conn);
+                adduser.Connection = conn;
+                conn.Open();
+                adduser.ExecuteReader();
+
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
+
+
--- RestoRapido/RestoRapido/API/ApiModifierController.cs
+++ RestoRapido/RestoRapido/API/ApiModifierController.cs
@@ -10,25 +10,74 @@ using System.Web.Http;
 
 namespace RestoRapido.Controllers
 {
-        public class ApiModifierController : ApiController
+    public class ApiModifierController : ApiController
+    {
+        [HttpGet]
+        public List<object> Modif(int id, string login, bool boBle, bool boLait, bool boOeuf, bool boArachide, bool boSoja, bool boFruitCoque, bool boPoisson, bool boSesame, bool boCrustace, bool boMollusque)
         {
-        /*
-            public List<object> Modif(int id, string login,bool boBle,bool boLait, bool boOeuf,bool boArachide, bool boSoja, bool boFruitCoque, bool boPoisson, bool boSesame, bool boCrustace, bool boMollusque)
-            {
-       
-                List<object> results = new List<object>();
 
-                SqlConnection conn = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbRestoRapidoV26.mdf;Initial Catalog=RestoRapido;Integrated Security=True");
-                SqlCommand modifuser = new SqlCommand("SELECT UtilisateurID,UtilisateurType,UtilisateurPrenom,UtilisateurNom,UtilisateurNomUsager,m_boBle,m_boLait,m_boOeuf,m_boArachide,m_boSoja,m_boFruitCoque,m_boPoisson,m_boSesame,m_boCrustace,m_boMollusque FROM Utilisateurs WHERE UtilisateurNomUsager = '" + login + "' AND UtilisateurMDP = '" + CEncryption.CalculateMD5Hash(mdp) + "'", conn);
+
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbRestoRapidoV26.mdf;Initial Catalog=RestoRapido;Integrated Security=True");
+            SqlCommand checkuser = new SqlCommand("SELECT * FROM Utilisateurs WHERE UtilisateurNomUsager = '" + login + "'", conn);
+            checkuser.Connection = conn;
+            conn.Open();
+            SqlDataReader dr = checkuser.ExecuteReader();
+
+            List<object> results = new List<object>();
+
+            if (dr.HasRows)
+            {
+                conn.Close();
+                conn.Open();
+                SqlCommand modifuser = new SqlCommand("UPDATE Utilisateurs SET m_boBle = '" + boBle + "', m_boLait = '" + boLait + "', m_boOeuf = '" + boOeuf + "', m_boArachide = '" + boArachide + "', m_boSoja = '" + boSoja + "', m_boFruitCoque = '" + boFruitCoque + "', m_boPoisson = '" + boPoisson + "', m_boSesame = '" + boSesame + "', m_boCrustace = '" + boCrustace + "', m_boMollusque = '" + boMollusque + "' WHERE UtilisateurID = " + id , conn);
 
                 modifuser.Connection = conn;
+                modifuser.ExecuteReader();
+
+                results.Add(login);
+                results.Add(boBle);
+                results.Add(boLait);
+                results.Add(boOeuf);
+                results.Add(boArachide);
+                results.Add(boSoja);
+                results.Add(boFruitCoque);
+                results.Add(boPoisson);
+                results.Add(boSesame);
+                results.Add(boCrustace);
+                results.Add(boMollusque);
+
+
+
+            }
+
+            else {
+                conn.Close();
                 conn.Open();
-                SqlDataReader dr = modifuser.ExecuteReader();
+                
+                SqlCommand modifuser = new SqlCommand("UPDATE Utilisateurs SET UtilisateurNomUsager = '" + login + "', m_boBle = '" + boBle + "', m_boLait = '" + boLait + "', m_boOeuf = '" + boOeuf + "', m_boArachide = '" + boArachide + "', m_boSoja = '" + boSoja + "', m_boFruitCoque = '" + boFruitCoque + "', m_boPoisson = '" + boPoisson + "', m_boSesame = '" + boSesame + "', m_boCrustace = '" + boCrustace + "', m_boMollusque = '" + boMollusque + "' WHERE UtilisateurID = " + id, conn);
 
+                modifuser.Connection = conn;
+                modifuser.ExecuteReader();
+
+                results.Add(login);
+                results.Add(boBle);
+                results.Add(boLait);
+                results.Add(boOeuf);
+                results.Add(boArachide);
+                results.Add(boSoja);
+                results.Add(boFruitCoque);
+                results.Add(boPoisson);
+                results.Add(boSesame);
+                results.Add(boCrustace);
+                results.Add(boMollusque);
+
+                
+
+            }
+            conn.Close();
             return results;
-
-            }*/
         }
+    }
 }
 
 
